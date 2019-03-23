0021eb9c0e3d400daca871626e9235272948aac1
SuyashMore <suyash.21.more@gmail.com>
2019-03-23 11:36:20 +0530

    Added Meta-data

--- alias.sh
+++ alias.sh
@@ -1,8 +1,12 @@
 
 alias studio="cd ~/Downloads/android-studio/bin; ./studio.sh"
+
+# Git Aliases
 alias gp="git push"
 alias gc="git add . ; git commit -m "
 
+# USB PORT Aliases
+alias tty="ls /dev/tty*"
 alias permit0="sudo chmod 777 /dev/ttyUSB0"
 alias permit1="sudo chmod 777 /dev/ttyUSB1"
 alias permit2="sudo chmod 777 /dev/ttyUSB2"
