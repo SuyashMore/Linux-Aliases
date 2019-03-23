9f047f70179bb66aee08f1d0bedc759b75152350
SuyashMore <suyash.21.more@gmail.com>
2019-03-23 11:31:42 +0530

    Added Alias File

---
+++ alias.sh
@@ -0,0 +1,11 @@
+
+alias studio="cd ~/Downloads/android-studio/bin; ./studio.sh"
+alias gp="git push"
+alias gc="git add . ; git commit -m "
+
+alias permit0="sudo chmod 777 /dev/ttyUSB0"
+alias permit1="sudo chmod 777 /dev/ttyUSB1"
+alias permit2="sudo chmod 777 /dev/ttyUSB2"
+alias permit3="sudo chmod 777 /dev/ttyUSB3"
+alias permit4="sudo chmod 777 /dev/ttyUSB4"
+alias permit5="sudo chmod 777 /dev/ttyUSB5"
