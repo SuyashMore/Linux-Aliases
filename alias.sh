5a4eaa10b51426eccdea20cf82139d5fd18edaad
SuyashMore <suyash.21.more@gmail.com>
2019-04-17 16:41:46 +0530

    Cleanup

--- alias.sh
+++ alias.sh
@@ -1,6 +1,4 @@
 
-alias studio="cd ~/Downloads/android-studio/bin; ./studio.sh"
-
 
 # Folder and File Aliases
 alias del="sudo rm -r"
@@ -11,8 +9,6 @@ alias gpo="git push origin"
 alias gc="git add . ; git commit -m "
 alias glog="git log --oneline "
 alias glogL="git log"
-
-
 
 # USB PORT Aliases
 alias tty="ls /dev/tty*"
@@ -25,11 +21,7 @@ alias permit5="sudo chmod 777 /dev/ttyUSB5"
 alias ttylog="sudo dmesg | grep tty"
 
 #ROS Aliases and SSHs
-export ROS_IP="192.168.1.107"
-alias expr='export ROS_MASTER_URI="http://192.168.1.118:11311"'
-alias avitra='ssh avitra@192.168.1.118'
 alias ranger='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
-alias jetson="ssh nvidia@192.168.1.136"
 alias gui="nautilus"
 alias wall='cmatrix -s -b;clear'
 alias update="sudo apt-get update"
